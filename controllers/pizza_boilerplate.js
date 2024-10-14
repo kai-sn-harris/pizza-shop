22543a8877bf8c1effdedf8e2158016286f6c620
Kai Harris <kai.sn.harris@gmail.com>
2024-10-14 21:51:08 +1100

    pizza data from menu

--- controllers/pizza_boilerplate.js
+++ controllers/pizza_boilerplate.js
@@ -1,9 +1,234 @@
 module.exports = {
     0: {
-        class: "traditional",
-        pizza_id: 0,
-        name: "house special",
-        vegetarian: false,
-        toppings: ["tomato", "cheese", "ham", "mushroom", "capsicum", "pineapple"]
+      class: "traditional",
+      pizza_id: 0,
+      name: "house special",
+      vegetarian: false,
+      toppings: ["tomato", "cheese", "ham", "mushroom", "capsicum", "pineapple"]
+    },
+    1: {
+      class: "traditional",
+      pizza_id: 1,
+      name: "capricciosa",
+      vegetarian: false,
+      toppings: ["tomato", "cheese", "ham", "mushrooms", "olives", "anchovies"]
+    },
+    2: {
+      class: "traditional",
+      pizza_id: 2,
+      name: "margherita",
+      vegetarian: true,
+      toppings: ["tomato", "cheese", "oregano"]
+    },
+    3: {
+      class: "traditional",
+      pizza_id: 3,
+      name: "napolitana",
+      vegetarian: true,
+      toppings: ["tomato", "cheese", "olives", "anchovies", "oregano"]
+    },
+    4: {
+      class: "traditional",
+      pizza_id: 4,
+      name: "americana",
+      vegetarian: false,
+      toppings: ["tomato", "cheese", "hot salami"]
+    },
+    5: {
+      class: "traditional",
+      pizza_id: 5,
+      name: "marinara",
+      vegetarian: false,
+      toppings: ["tomato", "cheese", "prawns", "baby clams", "anchovies", "garlic"]
+    },
+    6: {
+      class: "traditional",
+      pizza_id: 6,
+      name: "mexicana",
+      vegetarian: false,
+      toppings: ["tomato", "cheese", "hot salami", "mushrooms", "capsicum", "olives", "chili"]
+    },
+    7: {
+      class: "traditional",
+      pizza_id: 7,
+      name: "vegetarian",
+      vegetarian: true,
+      toppings: ["tomato", "cheese", "mushrooms", "capsicum", "onion", "olives"]
+    },
+    8: {
+      class: "traditional",
+      pizza_id: 8,
+      name: "hawaiian",
+      vegetarian: false,
+      toppings: ["tomato", "cheese", "ham", "pineapple"]
+    },
+    9: {
+      class: "traditional",
+      pizza_id: 9,
+      name: "chicken",
+      vegetarian: false,
+      toppings: ["tomato", "cheese", "chicken", "capsicum", "onion", "BBQ sauce"]
+    },
+    10: {
+      class: "traditional",
+      pizza_id: 10,
+      name: "the lot",
+      vegetarian: false,
+      toppings: ["everything"]
+    },
+    11: {
+      class: "traditional",
+      pizza_id: 11,
+      name: "aussie",
+      vegetarian: false,
+      toppings: ["tomato", "cheese", "ham", "bacon", "egg"]
+    },
+    12: {
+      class: "traditional",
+      pizza_id: 12,
+      name: "beef and bacon",
+      vegetarian: false,
+      toppings: ["tomato", "cheese", "ham", "beef", "bacon", "onion"]
+    },
+    13: {
+      class: "traditional",
+      pizza_id: 13,
+      name: "sandy supreme",
+      vegetarian: false,
+      toppings: ["tomato", "cheese", "ham", "mild salami", "mushrooms", "onions", "pineapple"]
+    },
+    14: {
+      class: "gourmet",
+      pizza_id: 14,
+      name: "sicilia",
+      vegetarian: true,
+      toppings: ["tomato", "mozzarella", "chili fennel Italian sausage", "semi-dried tomatoes", "green olives"]
+    },
+    15: {
+      class: "gourmet",
+      pizza_id: 15,
+      name: "cosenza",
+      vegetarian: true,
+      toppings: ["tomato", "mozzarella", "roasted field mushrooms", "broccolini", "fresh herbs"]
+    },
+    16: {
+      class: "gourmet",
+      pizza_id: 16,
+      name: "siena",
+      vegetarian: true,
+      toppings: ["tomato", "mozzarella", "eggplant", "roasted peppers", "sun-dried tomatoes", "marinated artichokes"]
+    },
+    17: {
+      class: "gourmet",
+      pizza_id: 17,
+      name: "milano",
+      vegetarian: false,
+      toppings: ["tomato", "mozzarella", "oven-roasted field mushrooms", "roasted peppers", "grilled zucchini", "sun-dried tomato", "parsley"]
+    },
+    18: {
+      class: "gourmet",
+      pizza_id: 18,
+      name: "nola",
+      vegetarian: true,
+      toppings: ["tomato", "mozzarella", "pumpkin", "sweet potato", "grilled zucchini", "fresh herbs"]
+    },
+    19: {
+      class: "gourmet",
+      pizza_id: 19,
+      name: "calabria",
+      vegetarian: true,
+      toppings: ["tomato", "mozzarella", "salami", "semi-dried tomatoes", "kalamata olives", "mixed herbs"]
+    },
+    20: {
+      class: "gourmet",
+      pizza_id: 20,
+      name: "bari",
+      vegetarian: false,
+      toppings: ["garlic base", "mozzarella", "king prawns", "assorted seafood", "garlic", "fresh herbs", "lemon wedge"]
+    },
+    21: {
+      class: "gourmet",
+      pizza_id: 21,
+      name: "parma",
+      vegetarian: false,
+      toppings: ["tomato", "mozzarella", "prosciutto crudo", "rocket", "shaved parmesan", "fresh herbs"]
+    },
+    22: {
+      class: "gourmet",
+      pizza_id: 22,
+      name: "tandoori",
+      vegetarian: false,
+      toppings: ["tomato", "mozzarella", "tandoori chicken", "pineapple", "Spanish onion", "mint and coriander"]
+    },
+    23: {
+      class: "gourmet",
+      pizza_id: 23,
+      name: "riviera",
+      vegetarian: false,
+      toppings: ["tomato", "mozzarella", "smoked salmon", "asparagus", "capers", "Spanish onion", "cracked pepper"]
+    },
+    24: {
+      class: "gourmet",
+      pizza_id: 24,
+      name: "feta",
+      vegetarian: true,
+      toppings: ["tomato", "feta", "kalamata olives", "roasted peppers", "sliced tomato"]
+    },
+    25: {
+      class: "gourmet",
+      pizza_id: 25,
+      name: "toscana",
+      vegetarian: false,
+      toppings: ["tomato", "mozzarella", "marinated chicken pieces", "roasted peppers", "Spanish onion", "cracked pepper"]
+    },
+    26: {
+      class: "gourmet",
+      pizza_id: 26,
+      name: "florentine",
+      vegetarian: true,
+      toppings: ["tomato", "mozzarella", "spinach", "pumpkin", "blue cheese"]
+    },
+    27: {
+      class: "gourmet",
+      pizza_id: 27,
+      name: "funghi",
+      vegetarian: false,
+      toppings: ["tomato", "mozzarella", "mushrooms", "Spanish onion", "parmesan", "cracked pepper"]
+    },
+    28: {
+      class: "gourmet",
+      pizza_id: 28,
+      name: "rosemary lamb",
+      vegetarian: false,
+      toppings: ["tomato", "mozzarella", "slow-cooked lamb", "roasted peppers", "spring onions", "tzatziki"]
+    },
+    29: {
+      class: "gourmet",
+      pizza_id: 29,
+      name: "satay chicken",
+      vegetarian: false,
+      toppings: ["mozzarella", "satay chicken", "capsicum", "onion"]
+    },
+    30: {
+      class: "gourmet",
+      pizza_id: 30,
+      name: "figaro",
+      vegetarian: false,
+      toppings: ["garlic base", "mozzarella", "leg ham", "caramelised onions", "fig", "blue cheese"]
+    },
+    31: {
+      class: "gourmet",
+      pizza_id: 31,
+      name: "hot honey",
+      vegetarian: false,
+      toppings: ["tomato", "mozzarella", "calabrese salami", "honey", "bocconcini", "spicy herbs", "basil"]
+    },
+    32: {
+      class: "gourmet",
+      pizza_id: 32,
+      name: "bruschetta",
+      vegetarian: true,
+      toppings: ["fresh diced tomatoes", "red onions", "basil", "balsamic glaze"]
     }
-};
+}
+  
